fd1cc41176ff78cbc5de38207698538f343e37aa
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-11 18:40:06 +0200

    Update font family to 'Onest' for the entire application

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        onest: ['"Onest"', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
