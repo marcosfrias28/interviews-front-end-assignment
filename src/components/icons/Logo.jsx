ce7df8df0a19c40f79be60ee2c3d0645ee240654
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-11 19:33:25 +0200

    Logo converted to React Component to improve code quality and performance on any route

---
+++ src/components/icons/Logo.jsx
@@ -0,0 +1,51 @@
+const Logo = (props) => (
+  <svg
+  width='50'
+    viewBox='0 0 512 512'
+    {...props}
+  >
+    <path
+      d='M376.271 0H28.935v512H483.06V0z'
+      style={{
+        fill: '#ee2712'
+      }}
+    />
+    <path
+      d='M376.276 0h106.789v512H376.276z'
+      style={{
+        fill: '#f7ee00'
+      }}
+    />
+    <path
+      d='M319.353 176.479 277.4 150.992l-41.956 25.487V0h83.909z'
+      style={{
+        fill: '#fb7100'
+      }}
+    />
+    <path
+      d='M28.935 0h173.672v512H28.935z'
+      style={{
+        fill: '#ff5d3d'
+      }}
+    />
+    <path
+      d='M277.398 0h-41.954v176.479l41.954-25.487z'
+      style={{
+        fill: '#ff9845'
+      }}
+    />
+    <path
+      d='M376.276 0h53.394v512h-53.394z'
+      style={{
+        fill: '#fffc99'
+      }}
+    />
+    <path
+      d='M413.997 0h31.347v260.681h-31.347zM413.997 316.123h31.347v124.27h-31.347z'
+      style={{
+        fill: '#ff9845'
+      }}
+    />
+  </svg>
+)
+export default Logo
