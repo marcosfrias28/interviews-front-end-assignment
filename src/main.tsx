22b112d7509b398047b6f5a09f2a018b32dc3d09
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-13 18:26:43 +0200

    - Refactor routes
    This commit refactors the routes in the main.tsx file to use separate route components for each page.

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import ReactDOM from 'react-dom/client'
-import { HomePage, SearchPage, Welcome, Details } from './App.tsx'
 import './index.css'
 import 'flowbite'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import React from 'react'
+import WelcomePage from './routes/Welcome'
+import HomePage from './routes/Home'
+import SearchPage from './routes/Search'
+import DetailsPage from './routes/Details'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Welcome />} />
+      <Route path='/' element={<WelcomePage />} />
       <Route path='/home' element={<HomePage />} />
       <Route path='/search' element={<SearchPage />} />
-      <Route path='/recipes/:id' element={<Details />} />
+      <Route path='/recipes/:id' element={<DetailsPage />} />
       <Route path='*' element={<h1>404</h1>} />
       <></>
     </Routes>
