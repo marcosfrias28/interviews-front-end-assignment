995fe9c44906153d6266cb6c1505dd9bc638b811
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-14 14:42:44 +0200

    chore: Refactor recipeStore and add search functionality

--- src/store/recipeStore.ts
+++ src/store/recipeStore.ts
@@ -3,15 +3,21 @@ import { create } from "zustand";
 import { persist, devtools } from "zustand/middleware";
 import { API_URL } from "../App";
 import { toast } from "sonner";
-import { cuisineType, difficultyType, filterType, recipeType, dietType } from "../types/api-types";
-
+import {
+  cuisineType,
+  difficultyType,
+  filterType,
+  recipeType,
+  dietType,
+  filterSearchType,
+} from "../types/api-types";
 
 export interface recipeStoreTypes {
- //State
+  //State
   filter: filterType;
   recipes: recipeType[] | [];
-  cuisines: cuisineType[]  | [];
-  difficulties: difficultyType[]  | [];
+  cuisines: cuisineType[] | [];
+  difficulties: difficultyType[] | [];
   diets: dietType[] | [];
   searchResults: recipeType[] | [];
   loading: boolean;
@@ -27,10 +33,8 @@ export interface recipeStoreTypes {
 
   //Getter functions
   getRecipes: (_limit: number, _page: number) => void;
-  getRecipeBy: (
-    filter: filterType,
-    input: string
-  ) => void;
+  getRecipeBy: (filter: filterType, input: string) => void;
+  getRecipeBySearch: (filter: filterSearchType, input: string) => void;
   getDifficulties: () => void;
   getCuisines: () => void;
   getDiets: () => void;
@@ -38,9 +42,10 @@ export interface recipeStoreTypes {
 
 export const useRecipeStore = create<recipeStoreTypes>()(
   devtools(
-    persist((set, get) => ({
+    persist(
+      (set, get) => ({
         //State
-        filter: "q",
+        filter: {q: "", cuisineId: "", difficultyId: "", dietId: ""},
         recipes: [],
         cuisines: [],
         difficulties: [],
@@ -50,41 +55,90 @@ export const useRecipeStore = create<recipeStoreTypes>()(
 
         //Setter functions
         setFilter: (filter) => set({ filter }, false, "set filter: " + filter),
-        setLoading: (loading: boolean) => set({ loading }, false, "set loading: " + loading),
-        setRecipes: (recipes: recipeType[]) => set({ recipes }, false, "set loading: " + recipes),
-        setDifficulties: (difficulties: difficultyType[]) => set({difficulties}, false, "set difficulties: " + difficulties),
-        setDiets: (diets: dietType[]) => set({diets}, false, "set diets: " + diets),
-        setCuisines: (cuisines: cuisineType[]) => set({ cuisines }, false, "set cuisines: " + cuisines),
-        setSearchResults: (searchResults: recipeType[]) => set({ searchResults }, false, "set searchResults: " + searchResults),
+        setLoading: (loading: boolean) =>
+          set({ loading }, false, "set loading: " + loading),
+        setRecipes: (recipes: recipeType[]) =>
+          set({ recipes }, false, "set loading: " + recipes),
+        setDifficulties: (difficulties: difficultyType[]) =>
+          set({ difficulties }, false, "set difficulties: " + difficulties),
+        setDiets: (diets: dietType[]) =>
+          set({ diets }, false, "set diets: " + diets),
+        setCuisines: (cuisines: cuisineType[]) =>
+          set({ cuisines }, false, "set cuisines: " + cuisines),
+        setSearchResults: (searchResults: recipeType[]) =>
+          set({ searchResults }, false, "set searchResults: " + searchResults),
 
         //Getter functions
         getRecipes: (_limit = 12, _page = 1) => {
           const { setRecipes, setLoading } = get();
           setLoading(true);
-          axios.get(`${API_URL}/recipes?_limit=${_limit}&_page=${_page}`).then(({data}) => setRecipes(data)).finally(() => setLoading(false));
+          axios
+            .get(`${API_URL}/recipes?_limit=${_limit}&_page=${_page}`)
+            .then(({ data }) => setRecipes(data))
+            .finally(() => setLoading(false));
         },
 
         getDiets: () => {
           const { setDiets } = get();
-          axios.get(`${API_URL}/diets`).then(({data}) => setDiets(data)).catch((e) => toast.error('Something went wrong getting "diets"'))
-         },
-
-        getDifficulties: () => {
-            const { setDifficulties } = get();
-            axios.get(`${API_URL}/difficulties`).then(({data}) => setDifficulties(data)).catch((e) => toast.error('Something went wrong getting "difficulties"'))
+          axios
+            .get(`${API_URL}/diets`)
+            .then(({ data }) => setDiets(data))
+            .catch((e) => toast.error('Something went wrong getting "diets"'));
         },
 
-        getRecipeBy: (filter, input) => {
+        getDifficulties: () => {
+          const { setDifficulties } = get();
+          axios
+            .get(`${API_URL}/difficulties`)
+            .then(({ data }) => setDifficulties(data))
+            .catch((e) =>
+              toast.error('Something went wrong getting "difficulties"')
+            );
+        },
+
+        getRecipeBy: ({ cuisineId, difficultyId, dietId }) => {
+          const { setSearchResults, setLoading } = get();
+          setLoading(true);
+          let QUERY = `${API_URL}/recipes?`;
+          // Filter recipes by name, cuisine, difficulty, or dietary
+          console.log(cuisineId, difficultyId, dietId);
+          if (cuisineId !== '') QUERY += `cuisineId=${cuisineId}&`;
+          if (difficultyId !== '') QUERY += `difficultyId=${difficultyId}&`;
+          if (dietId !== '') QUERY += `dietId=${dietId}&`;
+          axios
+            .get(QUERY)
+            .then(({ data }) => setSearchResults(data))
+            .catch((e) =>
+              toast.error('Something went wrong getting "Recipies"')
+            )
+            .finally(() => setLoading(false));
+        },
+
+        getRecipeBySearch: (filter : filterSearchType, input : string) => {
           const { setSearchResults, setLoading } = get();
           setLoading(true);
           // Filter recipes by name, cuisine, difficulty, or dietary
-          axios.get(`${API_URL}/recipes?${filter}=${input}`).then(({data}) => setSearchResults(data)).catch((e) => toast.error('Something went wrong getting "Recipies"')).finally(() => setLoading(false));
+          axios
+            .get(
+              `${API_URL}/recipes?${filter}=${input}`
+            )
+            .then(({ data }) => setSearchResults(data))
+            .catch((e) =>
+              toast.error('Something went wrong getting "Recipies"')
+            )
+            .finally(() => setLoading(false));
         },
 
         getCuisines: () => {
           const { setCuisines, setLoading } = get();
           setLoading(true);
-          axios.get(`${API_URL}/cuisines`).then(({data}) => setCuisines(data)).catch((e) => toast.error('Something went wrong getting "cuisines"')).finally(() => setLoading(false));
+          axios
+            .get(`${API_URL}/cuisines`)
+            .then(({ data }) => setCuisines(data))
+            .catch((e) =>
+              toast.error('Something went wrong getting "cuisines"')
+            )
+            .finally(() => setLoading(false));
         },
       }),
       { name: "recipeStore" }
