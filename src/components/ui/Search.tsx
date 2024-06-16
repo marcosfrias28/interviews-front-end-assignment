ea1e2d039182ee984c0ffcb265d1668d6de4f240
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-16 22:59:37 +0200

    - Search Component Linted - On search Route New Dependency for every flag, some responsive layout adjustments done, Document Linted

---
+++ src/components/ui/Search.tsx
@@ -0,0 +1,129 @@
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import useRecipeStore from "../../hooks/useRecipeStore";
+import SearchIcon from "../icons/search";
+import React from "react";
+import {
+  cuisineType,
+  dietType,
+  difficultyType,
+  filterSearchType,
+  filterType,
+} from "../../types/api-types";
+import Datalists from "./Datalists";
+
+interface Props {
+  mobile?: boolean;
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
+}
+
+function Search({ mobile }: Props) {
+  //Local states, used only in this component
+  const [input, setInput] = useState<string>("");
+
+  /*
+  Getting all necessary states and functions from the store using zustand
+  IMPORTANT NOT GETTING THE WHOLE STATE AT ONCE, JUST THE ONES NEEDED TO AVOID RE-RENDERING
+   -------------------------------------------------------------------------------------------------------------------------
+  */
+  const {
+    list,
+    setList,
+    diets,
+    getDiets,
+    difficulties,
+    getDifficulties,
+    filter,
+    setFilter,
+    searchResults,
+    cuisines,
+  } = useRecipeStore();
+  // ----------------------------------------------------------------------------------------------------------------------
+
+  // Fetching all recipes and cuisines on component mount
+  useEffect(() => {
+    getDiets();
+    getDifficulties();
+  }, []);
+
+  // Resetting the input field when the filter change or the submition is made
+  useEffect(() => {
+    setInput("");
+  }, [filter, searchResults]);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    //Checking if the input is a cuisine, difficulty or diet
+    const cuisine = cuisines.find(
+      (cuisine: cuisineType) => cuisine.name === input
+    );
+    const difficulty = difficulties.find(
+      (difficulty: difficultyType) => difficulty.name === input
+    );
+    const diet = diets.find((diet: dietType) => diet.name === input);
+    //Setting the filter based on the input
+    if (list === "q") {
+      setFilter({ dietId: "", difficultyId: "", cuisineId: "", q: input });
+    } else if (list === "cuisineId" && cuisine) {
+      setFilter({ q: "", dietId: "", difficultyId: "", cuisineId: cuisine.id });
+    } else if (list === "difficultyId" && difficulty) {
+      setFilter({
+        q: "",
+        dietId: "",
+        cuisineId: "",
+        difficultyId: difficulty.id,
+      });
+    } else if (list === "dietId" && diet) {
+      setFilter({ cuisineId: "", q: "", difficultyId: "", dietId: diet.id });
+    }
+
+    setInput("");
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      action="/search"
+      className={
+        !mobile
+          ? "relative hidden md:block w-full"
+          : "relative md:hidden w-full"
+      }
+    >
+      <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <SearchIcon />
+        <span className="sr-only">Search icon</span>
+      </div>
+      <input
+        list={list}
+        autoComplete="off"
+        id="search-navbar"
+        className="block p-2 ps-10 pe-32 text-sm w-full text-gray-900 border-yellow-500 rounded-full bg-gray-100 focus:ring-yellow-500 focus:border-yellow-500 outline-none"
+        placeholder="Search..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <Datalists />
+
+      {/* DESKTOP FILTER SELECT */}
+      <div className="absolute inset-y-0 end-0 bg-transparent ">
+        <select
+          onChange={(e) => setList(e.target.value as filterSearchType)}
+          className="text-sm w-32 h-full bg-transparent border border-black/20 rounded-tr-full rounded-br-full outline-0 focus:ring-0 focus:ring-yellow-500 focus:border-yellow-500"
+          value={list}
+          aria-placeholder="Search by..."
+          name=""
+          id=""
+        >
+          <option value="q">Name</option>
+          <option value="cuisineId">Cuisine</option>
+          <option value="difficultyId">Difficulty</option>
+          <option value="dietId">Dietary</option>
+        </select>
+      </div>
+    </form>
+  );
+}
+
+export default Search;
--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -2,8 +2,8 @@ import { MouseEventHandler, useEffect, useState } from "react";
 import useRecipeStore from "../hooks/useRecipeStore";
 import { Layout } from "../App";
 import { toast } from "sonner";
-import { Link } from "react-router-dom";
-import { Flag } from "../components/ui/FlagIcons";
+import { Link, useNavigate } from "react-router-dom";
+import { GetFlag } from "../components/ui/FlagIcons";
 import DifficultyLabel from "../components/ui/DifficultyLabel";
 import useTitle from "../hooks/useTitle";
 import { filterType } from "../types/api-types";
@@ -11,69 +11,83 @@ import { filterType } from "../types/api-types";
 function SearchPage() {
   // Setting the title of the page with simple custom hook
   useTitle("Search");
+  const navigate = useNavigate();
 
-    /*
+  /*
   Getting all necessary states and functions from the store using zustand
   IMPORTANT NOT GETTING THE WHOLE STATE AT ONCE, JUST THE ONES NEEDED TO AVOID RE-RENDERING
    -------------------------------------------------------------------------------------------------------------------------
   */
-  const {searchResults, cuisines, diets, getRecipeBy, filter, setFilter} = useRecipeStore();
+  const {
+    searchResults,
+    getCuisines,
+    cuisines,
+    diets,
+    getRecipeBy,
+    filter,
+    setFilter,
+  } = useRecipeStore();
   // ----------------------------------------------------------------------------------------------------------------------
 
   useEffect(() => {
+    getCuisines();
     getRecipeBy(filter);
   }, [filter]);
 
   function handleCuisineChange(e: React.ChangeEvent<HTMLInputElement>) {
     const value = e.currentTarget.getAttribute("value") as string;
-    setFilter({ ...filter, cuisineId: value });
+    setFilter({ ...filter, q: "", cuisineId: value });
   }
   function handleDifficultyClick(e: React.ChangeEvent<HTMLInputElement>) {
     const value = e.currentTarget.getAttribute("value") as string;
-    setFilter({ ...filter, difficultyId: value });
+    setFilter({ ...filter, q: "", difficultyId: value });
+  }
+
+  function handleClickRecipe(id: string) {
+    navigate(`/recipes/${id}`);
   }
 
   return (
     <Layout>
-      <h1 className="text-7xl font-bold text-center">
-        Search our <span className="italic">best </span> Recipes!
-      </h1>
-      <section className=" relative h-auto w-full flex flex-row gap-20">
-
-      <aside className="h-full sticky pt-20 bg-transparent w-1/5 z-10 top-0 left-0">
-        <h2 className="text-4xl font-semibold text-center">Filters</h2>
-        <section className="h-full w-full">
-          <div className="p-10">
-            <h3 className="text-xl font-semibold mb-5">Cuisines</h3>
-            <form action="">
-              {cuisines.map((cuisine) => (
-                <div key={cuisine.id}>
-                  <label
-                    htmlFor={cuisine.name}
-                    className=" has-[input:checked]:opacity-100 has-[input:checked]:scale-110 hover:scale-105 transition-transform opacity-80 text-sm font-medium flex items-center gap-2"
-                  >
-                    <Flag nationality={cuisine.id}></Flag> {cuisine.name}
-                    <input
-                      onChange={(e) => handleCuisineChange(e)}
-                      className="hidden"
-                      type="radio"
-                      name="cuisine"
-                      id={cuisine.name}
-                      value={cuisine.id}
-                    />
-                  </label>
-                </div>
-              ))}
-            </form>
-            <div className="flex flex-col gap-5 p-5">
-              <h3 className="text-xl font-semibold">Recipe Difficulty</h3>
-              <div className="flex flex-row gap-2">
-                <div className="flex flex-row gap-4">
+        <h1 className="text-7xl font-bold font-lato text-center mb-10">
+          Search our <span className="italic">best </span> Recipes!
+        </h1>
+        <section className="relative h-full w-full max-h-dvh max-w-screen-2xl grid grid-cols-[20%,1fr] gap-20 place-content-center px-10">
+          <aside className="flex flex-col h-full w-full static md:sticky col-span-[33%] place-self-start bg-[#f7f7f7] z-20 left-0 top-0">
+            <h2 className="text-4xl font-semibold font-lato text-center">Filters</h2>
+            <div className="">
+              <h3 className="text-xl font-semibold">Cuisines</h3>
+              <div className="w-full max-h-96 overflow-scroll overflow-x-hidden overflow-y-auto">
+                {cuisines.map((cuisine) => (
+                  <div key={cuisine.id}>
+                    <label
+                      htmlFor={cuisine.name}
+                      className=" has-[input:checked]:opacity-100 has-[input:checked]:translate-x-5 hover:scale-105 transition-transform opacity-80 text-sm font-medium flex items-center gap-1"
+                    >
+                      <GetFlag nationality={cuisine.id}></GetFlag>
+                      <span className="text-nowrap">{cuisine.name}</span>
+                      <input
+                        onChange={(e) => handleCuisineChange(e)}
+                        className="hidden"
+                        type="radio"
+                        name="cuisine"
+                        id={cuisine.name}
+                        value={cuisine.id}
+                      />
+                    </label>
+                  </div>
+                ))}
+              </div>
+              <div className="flex flex-col gap-2 pt-5 w-48">
+                <h3 className="text-xl font-semibold text-wrap">Recipe Difficulty</h3>
+                <div className="flex flex-row flex-wrap gap-2">
                   <label
                     htmlFor="difficulty"
                     className="has-[input:checked]:opacity-100 has-[input:checked]:scale-125 hover:scale-105 transition-transform opacity-60 text-sm font-medium flex items-center gap-2"
                   >
-                    <span className="pointer-events-none text-white bg-zinc-500 focus:ring-4 font-medium rounded-full text-sm px-5 py-1.5 text-center">All</span>
+                    <span className="pointer-events-none text-white bg-zinc-500 focus:ring-4 font-medium rounded-full text-sm px-5 py-1.5 text-center">
+                      All
+                    </span>
                     <input
                       onChange={(e) => handleDifficultyClick(e)}
                       className="hidden"
@@ -83,107 +97,110 @@ function SearchPage() {
                       value=""
                     />
                   </label>
-                {
-                  // Generate 3 buttons with difficulty levels
-                  [...Array(3)].map((_, i) => (
-                  <label
-                   key={i}
-                    htmlFor={'difficulty' + i}
-                    className=" has-[input:checked]:opacity-100 has-[input:checked]:scale-125 hover:scale-105 transition-transform opacity-80 text-sm font-medium flex items-center gap-2"
-                  >
-                    <DifficultyLabel difficulty={i + 1} />
-                    <input
-                      onChange={(e) => handleDifficultyClick(e)}
-                      className="hidden"
-                      type="radio"
-                      name="cuisine"
-                      id={'difficulty' + i}
-                      value={i + 1}
-                    />
-                  </label>
-                  ))
-                }
+                  {
+                    // Generate 3 buttons with difficulty levels
+                    [...Array(3)].map((_, i) => (
+                      <label
+                        key={i}
+                        htmlFor={"difficulty" + i}
+                        className=" has-[input:checked]:opacity-100 has-[input:checked]:scale-125 hover:scale-105 transition-transform opacity-80 text-sm font-medium flex items-center gap-2"
+                      >
+                        <DifficultyLabel difficulty={i + 1} />
+                        <input
+                          onChange={(e) => handleDifficultyClick(e)}
+                          className="hidden"
+                          type="radio"
+                          name="cuisine"
+                          id={"difficulty" + i}
+                          value={i + 1}
+                        />
+                      </label>
+                    ))
+                  }
                 </div>
               </div>
             </div>
-          </div>
+          </aside>
+
+          <section className="flex flex-col w-full overflow-scroll overflow-x-hidden col-span-[100%] text-center gap-10 drop-shadow-lg max-w-screen-xl">
+
+            {searchResults.length === 0 && (
+              <p className="mx-auto text-center">
+                No recipes found by your search inputs, try again
+              </p>
+            )}
+            {searchResults.length > 0 &&
+              searchResults.map((recipe, i) => {
+                if (!recipe.name) return toast.error("Something went wrong");
+                const {
+                  id,
+                  name,
+                  cuisineId,
+                  ingredients,
+                  instructions,
+                  difficultyId,
+                  image,
+                } = recipe;
+                const cuisine = cuisines?.find(
+                  (cuisine) => cuisine.id === cuisineId
+                );
+                return (
+                  <article
+                    key={id}
+                    onClick={() => handleClickRecipe(id)}
+                    className="flex flex-col cursor-pointer hover:scale-105 transition-transform lg:flex-row flex-nowrap max-w-screen-xl bg-zinc-100 rounded-xl p-5 "
+                  >
+                    <div className="relative w-full h-96 overflow-hidden rounded-xl lg:rounded-se-full lg:rounded-ee-full">
+                      <img
+                        className=" object-cover object-center w-full"
+                        src={`http://localhost:8080${image}`}
+                        alt={name}
+                      />
+                      <div className="absolute z-10 bottom-0 left-0 flex gap-5 m-3">
+                        <span className="flex items-center text-nowrap justify-center gap-2 text-sm font-medium text-white bg-gray-500/20 backdrop-blur-lg py-1 rounded-full px-3 italic mx-auto">
+                          <GetFlag nationality={cuisineId} />
+                          <span className="hidden lg:block">
+                            {cuisine?.name}
+                          </span>
+                        </span>
+                        <span className="flex items-center justify-center gap-2 text-sm font-medium text-white bg-gray-500/20 backdrop-blur-lg py-1 rounded-full px-3 italic mx-auto">
+                          <span className="hidden lg:block">Difficulty: </span>
+                          <DifficultyLabel difficulty={difficultyId} />
+                        </span>
+                      </div>
+                    </div>
+                    <aside>
+                      <h2 className="font-semibold text-2xl font-sans text-center ">
+                        {name}
+                      </h2>
+                      <div className="flex justify-between mb-7">
+                        <ul className="w-1/2">
+                          <li className="text-center font-semibold text-sm italic">
+                            Ingredients
+                          </li>
+                          {ingredients.map((ingredient: string, i: number) => (
+                            <li
+                              className=" list-inside list-item list-disc"
+                              key={i}
+                            >
+                              {ingredient}
+                            </li>
+                          ))}
+                        </ul>
+                        <div className=" bg-black w-[3px] mx-5" />
+                        <div className="w-1/2">
+                          <h3 className="text-center font-semibold text-sm italic">
+                            instructions
+                          </h3>
+                          <p>{instructions}</p>
+                        </div>
+                      </div>
+                    </aside>
+                  </article>
+                );
+              })}
+          </section>
         </section>
-      </aside>
-
-      <section className="flex flex-col relative top-0 mt-20 gap-10 drop-shadow-lg max-w-screen-xl">
-        {searchResults.length === 0 && (
-          <p className="text-center">
-            No recipes found by your search input, try again
-          </p>
-          )}
-          {searchResults.length > 0 &&
-            searchResults.map((recipe, i) => {
-              if (!recipe.name) return toast.error("Something went wrong");
-              const {
-                id,
-                name,
-                cuisineId,
-                ingredients,
-                instructions,
-                difficultyId,
-                image,
-              } = recipe;
-              const cuisine = cuisines?.find(
-                (cuisine) => cuisine.id === cuisineId
-              );
-              return (
-                <article
-                  key={id}
-                className="flex flex-col lg:flex-row flex-nowrap max-w-screen-xl bg-zinc-100 rounded-xl p-5 "
-              >
-                <div className="relative w-full h-full max-h-96 max-w-2xl overflow-hidden rounded-xl lg:rounded-se-full lg:rounded-ee-full">
-                  <Link to={`/recipes/${id}`}>
-                    <img
-                      className=" object-cover object-center w-full"
-                      src={`http://localhost:8080${image}`}
-                      alt={name}
-                    />
-                  </Link>
-                  <div className="absolute z-10 bottom-0 left-0 flex gap-5 m-3">
-                    <span className="flex items-center text-nowrap justify-center gap-2 text-sm font-medium border border-white text-white bg-gray-500/20 backdrop-blur-lg py-1 rounded-full px-3 italic mx-auto">
-                      <Flag nationality={cuisineId} />{" "}
-                      <span className="hidden lg:block"> {cuisine?.name}</span>
-                    </span>
-                    <span className="flex items-center justify-center gap-2 text-sm font-medium border border-white text-white bg-gray-500/20 backdrop-blur-lg py-1 rounded-full px-3 italic mx-auto">
-                      <span className="hidden lg:block">Difficulty: </span>
-                      <DifficultyLabel difficulty={difficultyId} />
-                    </span>
-                  </div>
-                </div>
-                <h2 className="font-semibold text-2xl font-sans text-center">
-                  {name}
-                </h2>
-                <div className="flex justify-between mb-7">
-                  <ul className="w-1/2">
-                    <li className="text-center font-semibold text-sm italic">
-                      Ingredients
-                    </li>
-                    {ingredients.map((ingredient: string, i: number) => (
-                      <li className=" list-inside list-item list-disc" key={i}>
-                        {ingredient}
-                      </li>
-                    ))}
-                  </ul>
-                  <div className=" bg-black w-[3px] mx-5" />
-                  <div className="w-1/2">
-                    <h3 className="text-center font-semibold text-sm italic">
-                      {" "}
-                      instructions
-                    </h3>
-                    <p>{instructions}</p>
-                  </div>
-                </div>
-              </article>
-            );
-          })}
-      </section>
-      </section>
-
     </Layout>
   );
 }
