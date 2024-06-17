1015ab3a2f32f4d29c1ca45e2d22fdd5eabab56a
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-17 08:25:05 +0200

    chore: Remove unused define block in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env.API_URL": JSON.stringify("http://localhost:8080"),
-  },
 });
