0f44fc016d29a60199f23741fe9d27a79adcda5c
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-17 23:55:51 +0200

    chore: Update Vite configuration for client build

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/interviews-front-end-assignment/client",
-  root: "client",
-  build: {
-    outDir: "../dist/client",
-  },
 });
